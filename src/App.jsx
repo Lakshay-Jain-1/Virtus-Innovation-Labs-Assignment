0ba01d78e5eb6eb5b1c8601fd0fe1301268c9644
Lakshay Jain <lakshaybusiness.12@gmail.com>
2024-09-04 17:47:39 +0530

    Added random object component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Perf } from "r3f-perf"
 import Road from './shared/components/Road'
 import Car from './shared/components/car'
 import { OrbitControls } from "@react-three/drei"
+import RandomObject from "./shared/components/RandomObject"
 
 
 
@@ -19,7 +20,7 @@ function App() {
       <OrbitControls />
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
-
+      <RandomObject/>
       <Road/>
       <Car/>
     
--- src/shared/components/RandomObject.jsx
+++ src/shared/components/RandomObject.jsx
@@ -1,14 +1,48 @@
+import { useFrame } from "@react-three/fiber";
+import { RigidBody } from "@react-three/rapier"
+import { useState } from "react";
 
 
 export default function RandomObject() {
-  
+   
+    let [display,setDisplay] = useState(0)
+   
+    let [z,setz] = useState(0)
+    useFrame(()=>{
+        if(display==0){
+            setDisplay(1)
+            setz(Math.random()*10)
+        }else{
+            setDisplay(0)
+            setz(Math.random()*10)
+        }
+    })
 
     return (
-        <>
+        <RigidBody type="dynamic" position={[0,4,0]} onCollisionEnter={()=>console.log(1)}>
+
+         {display==1?<Circle/>:<Square/>}
         
-        
-        </>
+        </RigidBody>
     )
 
 
 }
+
+function Circle() {
+    return (
+      <mesh>
+        <circleGeometry args={[1, 32]} /> {/* radius: 1, segments: 32 */}
+        <meshBasicMaterial color="blue" />
+      </mesh>
+    );
+  }
+
+  function Square() {
+    return (
+      <mesh>
+        <planeGeometry args={[1, 1]} /> {/* width: 1, height: 1 */}
+        <meshBasicMaterial color="green" />
+      </mesh>
+    );
+  }
